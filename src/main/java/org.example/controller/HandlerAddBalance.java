fd91f50fc3f6ae40d55e8a242017718e831c6512
a19215217 <19215217@sigma.sbrf.ru>
2021-06-01 10:18:13 +0500

    first task

--- src/main/java/org.example/controller/HandlerAddBalance.java
+++ src/main/java/org.example/controller/HandlerAddBalance.java
@@ -4,8 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.example.service.ServiceCard;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,21 +12,20 @@ public class HandlerAddBalance implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         Map<String, String> requestParamValue = null;
-        if ("GET".equals(httpExchange.getRequestMethod())) {
+        if ("POST".equals(httpExchange.getRequestMethod())) {
             requestParamValue = handleGetRequest(httpExchange);
 
         }
-        handleResponse(httpExchange,requestParamValue);
+        handleResponse(httpExchange, requestParamValue);
     }
 
-    private void handleResponse(HttpExchange httpExchange, Map<String,String> requestParamValue) {
+    private void handleResponse(HttpExchange httpExchange, Map<String, String> requestParamValue) {
         ServiceCard serviceCard = new ServiceCard();
         String card = requestParamValue.get("card");
         String cash = requestParamValue.get("balance");
-        serviceCard.addBalanceForCard(Long.valueOf(cash),card);
+        serviceCard.addBalanceForCard(Long.valueOf(cash), card);
         try {
-            byte[] bytes = ("Баланс карты №:"+card+" изменен").getBytes("utf-8");
-            //System.out.println(answer);
+            byte[] bytes = ("Баланс карты №:" + card + " изменен").getBytes("utf-8");
             httpExchange.sendResponseHeaders(200, bytes.length);
             OutputStream output = httpExchange.getResponseBody();
             output.write(bytes);
@@ -39,16 +37,35 @@ public class HandlerAddBalance implements HttpHandler {
     }
 
     private Map<String, String> handleGetRequest(HttpExchange httpExchange) {
-        String urlParam = httpExchange.
-                getRequestURI()
-                .toString().split("\\?")[1];
-        String[] params = urlParam.split("&");
-        Map<String, String> map = new HashMap<String, String>();
-        for (String param : params) {
-            String name = param.split("=")[0];
-            String value = param.split("=")[1];
-            map.put(name, value);
+//        String urlParam = httpExchange.
+//                getRequestURI()
+//                .toString().split("\\?")[1];
+        //String urlParam = httpExchange.getRequestBody().toString();
+        //System.out.println(urlParam);
+        InputStreamReader isr = null;
+        try {
+            isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            StringBuilder urlParam = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                urlParam.append(line);
+            }
+            br.close();
+            String[] params = urlParam.toString().split("&");
+            Map<String, String> map = new HashMap<String, String>();
+            for (String param : params) {
+                String name = param.split("=")[0];
+                String value = param.split("=")[1];
+                map.put(name, value);
+            }
+            return map;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return map;
+        return null;
     }
 }
+
